be67c001d061126c1901306c4d06d91efe62c79f
Fitria Rahmadani <91838067+ftriara@users.noreply.github.com>
2022-09-08 14:56:48 +0700

    Add files via upload

---
+++ Tugas Praktikum 2/LatihanKonstanta.java
@@ -0,0 +1,17 @@
+// file LatihanKonstanta.java
+public class LatihanKonstanta {
+    public static final int KURS_DOLLAR = 15000;
+    public static final double PI = 3.14;
+    public static final String NAMA = "Squidward";
+
+    public static void main(String args[]) {
+        //KURS_DOLLAR = 13500;
+
+        System.out.println(KURS_DOLLAR);
+        System.out.println(PI);
+        System.out.println(NAMA);
+    }
+}
+
+
+//Source code tersebut salah karena KURS_DOLLAR sudah merupakan konstanta jadi tidak dapat diubah nilainya
---
+++ Tugas Praktikum 2/Mahasiswa.java
@@ -0,0 +1,27 @@
+public class Mahasiswa {
+    String nim, nama, alamat;
+    char jenisKelamin;
+
+    // buatlah constructor untuk menginisiasi nilai dari variabel
+    // nim, nama, jenisKelamin, alamat
+
+    Mahasiswa(String nim, String nama, char jenisKelamin, String alamat) {
+        this.nim = nim;
+        this.nama = nama;
+        this.jenisKelamin = jenisKelamin;
+        this.alamat = alamat;
+    }
+
+    void printData() {
+        System.out.println("Data Mahasiswa :");
+        System.out.println("NIM           : " + nim);
+        System.out.println("Nama          : " + nama);
+        System.out.println("Jenis Kelamin : " + jenisKelamin);
+        System.out.println("Alamat        : " + alamat);
+    }
+
+    public static void main(String[] args) {
+        Mahasiswa mhs = new Mahasiswa("M0501001", "Patrick Star", 'L', "Bikini Bottom");
+        mhs.printData();
+    }
+}
