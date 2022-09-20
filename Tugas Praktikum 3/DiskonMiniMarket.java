7e01b4fdf702501e1c5e456a22fa232eda821b86
Fitria Rahmadani <91838067+ftriara@users.noreply.github.com>
2022-09-20 22:29:04 +0700

    Add files via upload

---
+++ Tugas Praktikum 3/DiskonMiniMarket.java
@@ -0,0 +1,44 @@
+// Nama :  Fitria Rahmadani
+// NIM  :  M0521022
+
+import java.util.Scanner;
+
+public class PPBO_03_Latihan1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Total pembelian : ");
+        
+        // tambahkan code untuk menyimpan nominal total pembelian
+        int totalBeli = sc.nextInt();
+
+        // tambahkan code untuk mengimplementasikan ketentuan diskon yang ditetapkan
+        double diskon;
+        int hargaDiskon;
+
+        if (totalBeli < 50000) {
+            diskon = 0f;
+        } else if (totalBeli >= 50000 && totalBeli <= 75000) {
+            diskon = 0.05f;
+        } else if (totalBeli >= 75000 && totalBeli <= 125000) {
+            diskon = 0.15f;
+        } else {
+            diskon = 0.20f;
+        }
+
+        // tambahkan code untuk menampilkan nominal yang harus dibayar 
+        // sesuai dengan ketentuan diskon yang diberikan
+        hargaDiskon = (int) (totalBeli - (totalBeli*diskon));
+        System.out.println("Total Pembelian      = Rp " + totalBeli);
+        System.out.println("Diskon Barang        = " + (int)(diskon*100) + "%");
+        System.out.println("Harga setelah diskon = Rp " + hargaDiskon);
+
+        sc.close();
+    }
+}
+
+// Seorang mahasiswa berbelanja di mini market. Mini market tersebut memberikan diskon kepada
+// pembeli dengan ketentuan
+// a. Diskon 0% jika total pembelian di bawah 50.000
+// b. Diskon 5% jika total pembelian 50.000 hingga 75.000
+// c. Diskon 15% jika total pembelian di atas 75.000 hingga 125.000
+// d. Diskon 20% jika total pembelian di atas 125.000
