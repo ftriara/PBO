9b17a6b6c6b85fe869c6392fa7927237aa97cc4e
Fitria Rahmadani <91838067+ftriara@users.noreply.github.com>
2022-09-20 22:11:18 +0700

    Add files via upload

---
+++ Tugas Praktikum 3/ForLoopDemo.java
@@ -0,0 +1,19 @@
+// file ForLoopDemo.java
+public class ForLoopDemo {
+    public static void main(String[] args) {
+        int arr[] = {5, 1, 4, 2, 3};
+
+        //ForLoop Sebelum
+        /*for (int i = 1; i < 6; i++) {
+            System.out.println("elemen ke-" + i + " = " + arr[i]);
+        }*/
+
+        //ForLoop Sesudah Perbaikan
+        for (int i = 0; i < 6; i++) {
+            System.out.println("elemen ke-" + i + " = " + arr[i]);
+        }
+    }
+}
+
+//Penulisan kode perulangan dengan for pada code tersebut belum tepat karena perulangan seharusnya dimulai dari
+//indeks ke-nol untuk dapat mengakses elemen pertama
