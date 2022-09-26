4be059cac90d0dad0cf65c07ebc217210cde67e6
Fitria Rahmadani <91838067+ftriara@users.noreply.github.com>
2022-09-26 21:01:39 +0700

    Update ForLoopDemoDoWhile.java

--- Tugas Praktikum 3/ForLoopDemoDoWhile.java
+++ Tugas Praktikum 3/ForLoopDemoDoWhile.java
@@ -2,9 +2,16 @@ public class ForLoopDemoDoWhile {
     public static void main(String[] args) {
         int arr[] = {5, 1, 4, 2, 3};
         int i = 0;
-        do {
+        
+         //ForLoop Sebelum
+        /*for (int i = 1; i < 6; i++) {
             System.out.println("elemen ke-" + i + " = " + arr[i]);
+        }*/
+
+        //Do While Sesudah Perbaikan
+        do {
+            System.out.println("elemen ke-" + (i+1) + " = " + arr[i]);
             i++;
-        } while (i < 6);
+        } while (i < 5);
     }
 }
