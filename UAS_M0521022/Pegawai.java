67e422f289643d2b385e4c9cb291c123177c41db
Fitria Rahmadani <91838067+ftriara@users.noreply.github.com>
2022-12-18 09:04:35 +0700

    Add files via upload

---
+++ UAS_M0521022/Pegawai.java
@@ -0,0 +1,11 @@
+// Nama  : Fitria Rahmadani
+// NIM   : M0521022
+// Kelas : Informatika A
+
+/**
+ * Interface Pegawai digunakan untuk membuat interface
+ * Memiliki method hitungGaji yang me-return nilai integer
+ */
+public interface Pegawai {
+    public int hitungGaji();   
+}
